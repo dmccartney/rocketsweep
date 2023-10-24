9c34063567c733ff6e51c44373612bc4872c066a
Daniel McCartney <dmccartney@gmail.com>
2023-10-23 17:29:15 -0700

    feat: add tooltip to ongoing rewards to explain a bit

--- web/src/components/ClaimButtonGroup.js
+++ web/src/components/ClaimButtonGroup.js
@@ -1,6 +1,6 @@
 import useK from "../hooks/useK";
 import useCanConnectedAccountWithdraw from "../hooks/useCanConnectedAccountWithdraw";
-import { Button, Skeleton } from "@mui/material";
+import { Button, Skeleton, Tooltip } from "@mui/material";
 import { CheckCircle, HourglassTop } from "@mui/icons-material";
 import _ from "lodash";
 import ClaimAndStakeForm from "./ClaimAndStakeForm";
@@ -23,16 +23,34 @@ export default function ClaimButtonGroup({
   }
   if (type !== "finalized") {
     return (
-      <Button size="small" disabled color="inherit" endIcon={<HourglassTop />}>
-        {_.capitalize(type)}
-      </Button>
+      <Tooltip title="This interval is still in progress. Reward amounts will continue to grow until they are finalized at the end of the interval.">
+        <span>
+          <Button
+            size="small"
+            disabled
+            color="inherit"
+            endIcon={<HourglassTop />}
+          >
+            {_.capitalize(type)}
+          </Button>
+        </span>
+      </Tooltip>
     );
   }
   if (isClaimed) {
     return (
-      <Button size="small" disabled color="inherit" endIcon={<CheckCircle />}>
-        Claimed
-      </Button>
+      <Tooltip title="Periodic rewards for this interval were already claimed.">
+        <span>
+          <Button
+            size="small"
+            disabled
+            color="inherit"
+            endIcon={<CheckCircle />}
+          >
+            Claimed
+          </Button>
+        </span>
+      </Tooltip>
     );
   }
   return (
