24c106a10565cae18be1275f051ce7a854c68a30
Daniel McCartney <dmccartney@gmail.com>
2024-01-18 04:56:22 -0500

    fix: fallback to hosted rewards tree when IPFS request fails

--- web/src/hooks/useFetchRewardSnapshots.js
+++ web/src/hooks/useFetchRewardSnapshots.js
@@ -1,16 +1,24 @@
 import useFetchJSONZST from "./useFetchJSONZST";
 import useSetting from "./useSetting";
 
+// HACK: for now, until we have a better fix for failed IPFS publications.
+const fallbackBase =
+  "https://storage.googleapis.com/lgtm-info-dev-preview/rewards-trees";
+// NOTE: we can't just use "https://github.com/rocket-pool/rewards-trees/raw/main"
+//       because GitHub doesn't support CORS for raw.githubusercontent.com.
+
 export default function useFetchRewardSnapshots({
   snapshots,
   network = "mainnet",
 }) {
   let [ipfsBase] = useSetting("ipfs.base");
   let snapshotJsons = useFetchJSONZST(
-    (snapshots || []).map(
-      ({ rewardIndex, merkleTreeCID }) =>
-        `${ipfsBase}/ipfs/${merkleTreeCID}/rp-rewards-${network}-${rewardIndex}.json.zst`
-    ),
+    (snapshots || []).map(({ rewardIndex, merkleTreeCID }) => ({
+      url: `${ipfsBase}/ipfs/${merkleTreeCID}/rp-rewards-${network}-${rewardIndex}.json.zst`,
+      fallbackUrls: [
+        `${fallbackBase}/${network}/rp-rewards-${network}-${rewardIndex}.json`,
+      ],
+    })),
     {
       enabled: !!ipfsBase,
       cacheTime: Math.Infinite,
