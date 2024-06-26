165e01f08f9277a320c3041e5eb86da947daa572
Daniel McCartney <dmccartney@gmail.com>
2024-06-26 12:20:40 -0500

    feat: update houston contracts

--- web/src/contracts.js
+++ web/src/contracts.js
@@ -8,9 +8,15 @@ import RocketNodeManager from "./generated/contracts/RocketNodeManager.json";
 import RocketRewardsPool from "./generated/contracts/RocketRewardsPool.json";
 import RocketStorageK from "./generated/contracts/RocketStorage.json";
 
+// This names, locates, and defines the interface to the contracts we use.
+// The address can change during a rocketpool upgrade. The first `address` is the latest address.
+// But we hang onto the old addresses for things like searching all event logs.
 const contracts = {
   RocketMerkleDistributorMainnet: {
-    address: "0x7eccbbd05830edf593d30005b8f69e965af4d59f",
+    address: [
+      "0x5cE71E603B138F7e65029Cc1918C0566ed0dBD4B",
+      "0x7eccbbd05830edf593d30005b8f69e965af4d59f",
+    ],
     abi: RocketMerkleDistributorMainnet.abi,
   },
   RocketMinipoolBase: {
@@ -21,14 +27,18 @@ const contracts = {
   },
   RocketMinipoolManager: {
     address: [
-      "0x6d010C43d4e96D74C422f2e27370AF48711B49bF", // latest
+      "0x09fbCE43e4021a3F69C4599FF00362b83edA501E",
+      "0x6d010C43d4e96D74C422f2e27370AF48711B49bF",
       "0x84d11b65e026f7aa08f5497dd3593fb083410b71",
       "0x6293b8abc1f36afb22406be5f96d893072a8cf3a",
     ],
     abi: RocketMinipoolManager.abi,
   },
   RocketNetworkPrices: {
-    address: "0x751826b107672360b764327631cc5764515ffc37",
+    address: [
+      "0x25E54Bf48369b8FB25bB79d3a3Ff7F3BA448E382",
+      "0x751826b107672360b764327631cc5764515ffc37",
+    ],
     abi: RocketNetworkPrices.abi,
   },
   RocketNodeDistributorInterface: {
@@ -36,11 +46,15 @@ const contracts = {
     abi: RocketNodeDistributorInterface.abi,
   },
   RocketNodeManager: {
-    address: "0x89F478E6Cc24f052103628f36598D4C14Da3D287",
+    address: [
+      "0x2b52479F6ea009907e46fc43e91064D1b92Fdc86",
+      "0x89F478E6Cc24f052103628f36598D4C14Da3D287",
+    ],
     abi: RocketNodeManager.abi,
   },
   RocketRewardsPool: {
     address: [
+      "0xEE4d2A71cF479e0D3d0c3c2C923dbfEB57E73111",
       "0xA805d68b61956BC92d556F2bE6d18747adAeEe82",
       "0x594Fb75D3dc2DFa0150Ad03F99F97817747dd4E1",
     ],
